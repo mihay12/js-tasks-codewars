e5f0c8155d486cf2cd363cd5b262f41c276ccf07
mihay <45318224+mihay12@users.noreply.github.com>
2020-05-01 20:47:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-//https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
+//1. https://www.codewars.com/kata/56dec885c54a926dcd001095/train/javascript
 
 function opposite(number) {
   return -number;
 }
+
+//2. https://www.codewars.com/kata/grasshopper-personalized-message/train/javascript?fbclid=IwAR2cvOagR1IBDMRgs5UfOuCNWQSlicpBkrAbClqwfPlZsOzQMBAnSnc3wzc
+
+function greet (name, owner) {
+  var helloBoss = 'Hello boss';
+  var helloGuest = 'Hello guest';
+    if(name == owner){
+      return helloBoss;
+    }else{
+      return helloGuest;
+    }
+}
+
+//3. https://www.codewars.com/kata/keep-hydrated-1/train/javascript?fbclid=IwAR0CYZEBklfGT9hiO-kqxDNIdeNZ8kNiKfHdCkQN6G_ZxEnpn3DcNEEbhDs
+
+function litres(time) {
+  var hours;
+  var water = 0.5;
+  hours = time * water;
+  hours = Math.floor(hours);
+  return hours;
+}
+
+//4. https://www.codewars.com/kata/grasshopper-messi-goals-function/train/javascript?fbclid=IwAR1iF0o2KwNjFVx59-cex6i56OdMYy8TXWtUw77Ft8L59HyP84XI9joOPxk
+
+
+//5. https://www.codewars.com/kata/grasshopper-terminal-game-move-function/train/javascript?fbclid=IwAR29SrTWeg6MYk20xGJdvXF-DlGR7thBN68MM_IkatcF7JQciWUKbvDevpQ
+
+
+//6. https://www.codewars.com/kata/be-concise-i-the-ternary-operator/train/javascript?fbclid=IwAR2b5zHabEvnfOPDA8XvVnGLbnCwGdWuMqC_AgOAsAyz_bCNb4t9b08bdbs
