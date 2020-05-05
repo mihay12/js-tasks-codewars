c4ebc0260d85e620c129926dd0b54f0e5b4eb545
mihay <45318224+mihay12@users.noreply.github.com>
2020-05-05 20:54:24 +0300

    Update 06-05-2020-index.js

--- 06-05-2020-index.js
+++ 06-05-2020-index.js
@@ -1,5 +1,4 @@
 //1. https://www.codewars.com/kata/577a98a6ae28071780000989/train/javascript?fbclid=IwAR0iHYt1qy8dONVPFaksjGoW-J6pYpKVkOEhoVxGEw6jiQ6v31VnmD4k_Es
-
 var min = function (list) {
   for (let i = 0; i < list.length; i++) {
     if (list[0] > list[i]) {
@@ -17,3 +16,13 @@ var max = function (list) {
   }
   return list[0];
 };
+
+
+//2. https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/javascript
+function maps(x) {
+  const array = [];
+  for (let i = 0; i < x.length; i++) {
+    array.push(x[i] * 2);
+  }
+  return array;
+}
