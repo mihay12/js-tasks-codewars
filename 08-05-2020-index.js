f19b925819ca7f6cbd469150a6715415ccd552c1
mihay <45318224+mihay12@users.noreply.github.com>
2020-05-07 20:14:18 +0300

    Create 08-05-2020-index.js

---
+++ 08-05-2020-index.js
@@ -0,0 +1,10 @@
+//1. 
+
+
+
+//Не з домашньої роботи
+//1. https://www.codewars.com/kata/571f1eb77e8954a812000837/train/javascript
+
+function animal(obj){
+  return "This " + obj.color + " " + obj.name +  " has " + obj.legs + " legs.";
+}
