0b54ac68de99801d8c2176a6b863a59f9ee1c707
mihay <45318224+mihay12@users.noreply.github.com>
2020-05-07 21:12:54 +0300

    Update 08-05-2020-index.js

--- 08-05-2020-index.js
+++ 08-05-2020-index.js
@@ -1,4 +1,34 @@
-//1. 
+//1. https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript?fbclid=IwAR1MPbyOBBBoHZ4BzwhzfDFzB1yUn2B7__-uNnOEbHEdUiDh-xivBJnRikc
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+
+//2. https://www.codewars.com/kata/geometry-basics-circle-circumference-in-2d/train/javascript?fbclid=IwAR2HjzuGNszxDqd0qTXUuNZ3XsHvnRWOk3WWw7YcfOsFK8cvvkXTwvG-x6U
+function circleCircumference(circle) {
+  const calculatingCircumFerence = 2 * Math.PI * circle.radius;
+  return calculatingCircumFerence;
+}
+
+//3. https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript?fbclid=IwAR3KuJtYpRl8jFy0CoQm8plHthmcZK2tiaAUDuMyN3Gy9m__YUhstG3JeDU
+
+
+//4. https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript?fbclid=IwAR37RsevfgYVA2q03XRFvguVjsBdjDUUght7W6sGRGFOr9ygl1i03Ps_Ye0
+function giveMeFive(obj){
+let arr = [];
+  for (var key in obj){
+    if(key.length === 5){
+      arr.push(key);
+    }
+    if(obj[key].length  === 5){
+      arr.push(obj[key]);
+    }
+  }
+  return arr;
+}
+
+//5. https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript?fbclid=IwAR3ZJF4owwIbuogxE2SnjaO5lhwGoHvdqZKWtHFjNme6i0max7Tdv6SFvIg
 
 
 
