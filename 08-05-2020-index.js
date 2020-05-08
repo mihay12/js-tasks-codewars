9efbc5542ea68ef85f402be05fdd24d7ed26af76
mihay <45318224+mihay12@users.noreply.github.com>
2020-05-08 15:13:45 +0300

    Update 08-05-2020-index.js

--- 08-05-2020-index.js
+++ 08-05-2020-index.js
@@ -12,6 +12,48 @@ function circleCircumference(circle) {
 }
 
 //3. https://www.codewars.com/kata/fun-with-es6-classes-number-2-animals-and-inheritance/train/javascript?fbclid=IwAR3KuJtYpRl8jFy0CoQm8plHthmcZK2tiaAUDuMyN3Gy9m__YUhstG3JeDU
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.legs = 0;
+    this.species = "shark";
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.legs = 4;
+    this.species = "cat";
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super();
+    this.name = name;
+    this.age = age;
+    this.status = status;
+    this.master = master;
+    this.legs = 4;
+    this.species = "dog";
+  }
+  introduce() {
+    return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+  }
+  greetMaster() {
+    return `Hello ${this.master}`;
+  }
+}
 
 
 //4. https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript?fbclid=IwAR37RsevfgYVA2q03XRFvguVjsBdjDUUght7W6sGRGFOr9ygl1i03Ps_Ye0
