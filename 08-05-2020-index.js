cfdc676fa7fe293fbfa38ac06da2b6b1dba7b8a7
mihay <45318224+mihay12@users.noreply.github.com>
2020-05-08 15:03:14 +0300

    Update 08-05-2020-index.js

--- 08-05-2020-index.js
+++ 08-05-2020-index.js
@@ -34,7 +34,6 @@ let arr = [];
 
 //Не з домашньої роботи
 //1. https://www.codewars.com/kata/571f1eb77e8954a812000837/train/javascript
-
 function animal(obj){
   return "This " + obj.color + " " + obj.name +  " has " + obj.legs + " legs.";
 }
