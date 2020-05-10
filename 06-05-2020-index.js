2927697728bf76960d2e6dd67e1ee213788379dd
mihay <45318224+mihay12@users.noreply.github.com>
2020-05-10 13:54:48 +0300

    Update 06-05-2020-index.js

--- 06-05-2020-index.js
+++ 06-05-2020-index.js
@@ -104,17 +104,3 @@ function DNAtoRNA(dna) {
 
   return rna.join("");
 }
-
-
-
-//Завдання не з домашньої роботи:
-//1. https://www.codewars.com/kata/5721a78c283129e416000999/train/javascript
-function pickIt(arr) {
-  var odd = [],
-    even = [];
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) even.push(arr[i]);
-    else odd.push(arr[i]);
-  }
-  return [odd, even];
-}
