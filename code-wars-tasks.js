a3f9b1e95a05b0036be5029146315d54d30ccedc
mihay <45318224+mihay12@users.noreply.github.com>
2020-05-24 16:11:01 +0300

    Update code-wars-tasks.js

--- code-wars-tasks.js
+++ code-wars-tasks.js
@@ -206,3 +206,13 @@ function arrayPlusArray(firstArray, secondArray) {
   }
   return sumArray;
 }
+
+//12. https://www.codewars.com/kata/57cc975ed542d3148f00015b/train/javascript
+function check(a, x) {
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] === x) {
+      return true;
+    }
+  }
+  return false;
+}
